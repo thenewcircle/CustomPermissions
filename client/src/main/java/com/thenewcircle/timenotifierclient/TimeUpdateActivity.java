59d5fdd0ea191dbd25aee0e4505172aadd76d2ed
Gil Zhaiek <gilzhaiek@gmail.com>
2018-04-09 23:40:07 +0200

    Updated to Android permission M and N

--- client/src/main/java/com/thenewcircle/timenotifierclient/TimeUpdateActivity.java
+++ client/src/main/java/com/thenewcircle/timenotifierclient/TimeUpdateActivity.java
@@ -1,9 +1,12 @@
 package com.thenewcircle.timenotifierclient;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,10 +14,12 @@ import android.widget.TextView;
 public class TimeUpdateActivity extends Activity {
     protected static final String ACTION_TOD_UPDATE = "com.thenewcircle.timenotifier.ACTION_TOD";
     protected static final String EXTRA_TOD_MS = "time-update-tod-ms";
+    private static final String PERMISSION_USE_TIME_NOTIFIER = "com.thenewcircle.timenotifier.USE_TIME_NOTIFIER";
+    private static final int PERMISSION_REQUEST_CODE = 123;
 
     private TextView mStatusText;
     private TextView mTimeText;
-    private TimeUpdateTickReceiver timeUpdateTickReceiver = new TimeUpdateTickReceiver();
+    private TimeUpdateTickReceiver mTimeUpdateTickReceiver = new TimeUpdateTickReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,7 @@ public class TimeUpdateActivity extends Activity {
 
         // Register the Broadcast Receiver with the Service Intent Action
         IntentFilter intentFilter = new IntentFilter("com.thenewcircle.timenotifier.ACTION_TICK");
-        registerReceiver(timeUpdateTickReceiver, intentFilter);
+        registerReceiver(mTimeUpdateTickReceiver, intentFilter);
 
         //  Get the instances of our status and time update text views
         mStatusText = (TextView) findViewById(R.id.status_info);
@@ -36,21 +41,57 @@ public class TimeUpdateActivity extends Activity {
         Intent intent = getIntent();
         String action = intent.getAction();
         if (action.equals(Intent.ACTION_MAIN)) {
-            mStatusText.setText(R.string.connecting);
-
-            Intent srvIntent = new Intent(Intent.ACTION_MAIN);
-            ComponentName srvComp = new ComponentName(
-                    "com.thenewcircle.timenotifier",
-                    "com.thenewcircle.timenotifier.TimeNotifierService");
-            srvIntent.setComponent(srvComp);
-
-            //  LAB: Verify the Activity crashes after the Service is updated to
-            //  require a permission to start/bind it.
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                startForegroundService(srvIntent);
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                if (checkSelfPermission(PERMISSION_USE_TIME_NOTIFIER)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Request the permission
+                    requestPermissions(new String[]{PERMISSION_USE_TIME_NOTIFIER},
+                            PERMISSION_REQUEST_CODE);
+                } else {
+                    // Permission has already been granted
+                    startTimeNotifierService();
+                }
             } else {
-                startService(srvIntent);
+                startTimeNotifierService();
             }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mTimeUpdateTickReceiver);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startTimeNotifierService();
+                } else {
+                    mStatusText.setText(R.string.permission_request_cancelled);
+                }
+            }
+        }
+    }
+
+    private void startTimeNotifierService() {
+        mStatusText.setText(R.string.connecting);
+
+        Intent srvIntent = new Intent(Intent.ACTION_MAIN);
+        ComponentName srvComp = new ComponentName(
+                "com.thenewcircle.timenotifier",
+                "com.thenewcircle.timenotifier.TimeNotifierService");
+        srvIntent.setComponent(srvComp);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            startForegroundService(srvIntent);
+        } else {
+            startService(srvIntent);
         }
     }
 
