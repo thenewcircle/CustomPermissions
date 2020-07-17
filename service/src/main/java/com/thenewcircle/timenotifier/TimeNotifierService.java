1fba2ab25875a7a238eb0e95306e250d2922d387
Gil Zhaiek <gilzhaiek@gmail.com>
2020-07-16 21:59:37 -0700

    Cleanup for Android 10

--- service/src/main/java/com/thenewcircle/timenotifier/TimeNotifierService.java
+++ service/src/main/java/com/thenewcircle/timenotifier/TimeNotifierService.java
@@ -8,7 +8,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
